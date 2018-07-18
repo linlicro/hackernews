ed27ffe236bc4d77f1a118dc0b5a211e41b79eb2
Lin Li <lilin@treefinance.com.cn>
2018-07-19 06:07:10 +0800

    map

--- src/App.js
+++ src/App.js
@@ -24,9 +24,10 @@ class App extends Component {
       <div className="App">
         {list.map(function (item) {
           return (
-            <div> <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
+            <div key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
               <span>{item.author}</span>
               <span>{item.num_comments}</span>
               <span>{item.points}</span>
