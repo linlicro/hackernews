5222f5818c794f4e616f4e82ae1b9d043d781648
Lin Li <lilin@treefinance.com.cn>
2018-07-28 14:40:52 +0800

    error

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 const DEFAULT_QUERY = "redux";
 const DEFAULT_HPP = "100";
 
-const PATH_BASE = "https://hn.algolia.com/api/v1";
+const PATH_BASE = "https://hn.algoliaaaa.com/api/v1";
 const PATH_SEARCH = "/search";
 const PARAM_SEARCH = "query=";
 const PARAM_PAGE = "page=";
@@ -17,7 +17,8 @@ class App extends Component {
     this.state = {
       results: null,
       searchKey: "",
-      searchTerm: DEFAULT_QUERY
+      searchTerm: DEFAULT_QUERY,
+      error: null
     };
 
     this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
@@ -65,7 +66,7 @@ class App extends Component {
     )
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
-      .catch(e => e);
+      .catch(e => this.setState({ error: e }));
   }
 
   componentDidMount() {
@@ -94,7 +95,7 @@ class App extends Component {
   }
 
   render() {
-    const { searchTerm, results, searchKey } = this.state;
+    const { searchTerm, results, searchKey, error } = this.state;
     const page =
       (results && results[searchKey] && results[searchKey].page) || 0;
     const list =
@@ -111,7 +112,13 @@ class App extends Component {
             Search
           </Search>
         </div>
-        {result && <Table list={list} onDismiss={this.onDismiss} />}
+        {error ? (
+          <div className="interactions">
+            <p>Something went wrong.</p>
+          </div>
+        ) : (
+          <Table list={list} onDismiss={this.onDismiss} />
+        )}
         <div>
           <Button
             onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}
