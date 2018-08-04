36e0469916df9f5dbf332d6e4c20ad896119eaf5
Lin Li <lilin@treefinance.com.cn>
2018-08-05 07:58:38 +0800

    stats

--- src/App.js
+++ src/App.js
@@ -21,6 +21,23 @@ const SORTS = {
   POINTS: list => sortBy(list, "points").reverse()
 };
 
+const updatedSearchTopStoriesState = (hits, page) => (prevState) => {
+  const { searchKey, results} = prevState;
+
+  const oldHits =
+    results && results[searchKey] ? results[searchKey].hits : [];
+
+  const updatedHits = [...oldHits, ...hits];
+  
+  return {
+    results: {
+      ...results,
+      [searchKey]: {hits: updatedHits, page}
+    },
+    isLoading: false
+  };
+};
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -57,20 +74,7 @@ class App extends Component {
 
   setSearchTopStories(result) {
     const { hits, page } = result;
-    const { searchKey, results } = this.state;
-
-    const oldHits =
-      results && results[searchKey] ? results[searchKey].hits : [];
-
-    const updatedHits = [...oldHits, ...hits];
-
-    this.setState({
-      results: {
-        ...results,
-        [searchKey]: { hits: updatedHits, page }
-      },
-      isLoading: false
-    });
+    this.setState(updatedSearchTopStoriesState(hits, page));
   }
 
   fetchSearchTopStories(searchTerm, page = 0) {
